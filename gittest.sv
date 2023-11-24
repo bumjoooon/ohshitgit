64a08225138fca8b6a5e5a35d7e80f2e5982012f
bumjoooon <kbj970505@naver.com>
2023-11-24 14:55:51 +0900

    problem 14 testcode start

--- gittest.sv
+++ gittest.sv
@@ -1,11 +1,28 @@
 module tb;
 
+class base_class 
+    int [7:0] a;
+    int [7:0] b;
+
+    new(int [7:0] a, int[7:0]b);
+
+    function sum (a, b)
+        return a+b;
+    endfunction
+
+    function mult(a, b)
+        return a*b;
+    endfunction
+
+endclass
 initial begin
 
     int a;
     int b;
     int c;
     reg [7:0] q;
+
+    tetstsetsetse salfjlkasf;
 
 end
 
