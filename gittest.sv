6cdec5e0b8e3750230a50e341b39289c5125a886
bumjoooon <kbj970505@naver.com>
2023-11-24 15:05:43 +0900

    make base class and child class

--- gittest.sv
+++ gittest.sv
@@ -1,29 +1,75 @@
-module tb;
 
 class base_class 
     int [7:0] a;
     int [7:0] b;
 
-    new(int [7:0] a, int[7:0]b);
+    function new(int [7:0] a, int[7:0]b);
+        this.a = a;
+        this.b = b;
+    endfunction
 
-    function sum (a, b)
+    function sum (a, b);
         return a+b;
     endfunction
 
-    function mult(a, b)
+    function mult(a, b);
         return a*b;
     endfunction
 
 endclass
+
+class input_odd extends base_class
+
+    function new();
+        super.new();
+    endfunction
+
+    function sum (a, b);
+        if ( (a%2)==1 or (b%2)==1) begin
+            return a+b;
+        end else
+            return 0;
+    endfunction
+
+    function mult (a, b);
+        if ( (a%2)==1 or (b%2)==1) begin
+            return a*b;
+        end else
+            return 0;
+    endfunction
+    
+endclass
+
+class output_odd extends base_class
+
+    function new();
+        super.new();
+    endfunction
+
+    function sum (a, b);
+        if ( ((a+b)%2)==1) begin
+            return a+b;
+        end else
+            return 0;
+    endfunction
+
+    function mult (a, b);
+        if ( ((a*b)%2)==1 ) begin
+            return a*b;
+        end else
+            return 0;
+    endfunction
+    
+endclass
+
+
 initial begin
 
-    int a;
-    int b;
-    int c;
+    int [7:0] a;
+    int [7:0] b;
+    int [7:0] c;
     reg [7:0] q;
 
-    tetstsetsetse salfjlkasf;
 
 end
 
-endmodule
